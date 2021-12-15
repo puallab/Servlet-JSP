827b0d1f40c0e606e79e29f3028b528c60fc6476
puallab <pdh5056@gmail.com>
2021-12-15 09:00:52 +0900

    Initial commit

---
+++ src/main/java/com/example/servletjsp/ServletjspApplication.java
@@ -0,0 +1,13 @@
+package com.example.servletjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletjspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletjspApplication.class, args);
+    }
+
+}
