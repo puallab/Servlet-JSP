654998fb2907008b4f3e5381a9adf5c8d569faaf
puallab <pdh5056@gmail.com>
2022-01-08 03:25:30 +0900

    Session 저장, 추출, 예제

---
+++ src/main/java/com/example/servletjsp/Servlet/SeesionTestServlet.java
@@ -0,0 +1,57 @@
+package com.example.servletjsp.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/sessionTest")
+public class SeesionTestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        String param = req.getParameter("p");
+        String msg = null;
+        HttpSession session = null;
+
+        if(param.equals("create")) {
+            session = req.getSession();
+            if (session.isNew()) msg = "새로운 객체 생성";
+            else msg = "기존의 세션 리턴";
+        }else if (param.equals("delete")){
+            session = req.getSession();
+            if(session != null){
+                session.invalidate();
+                msg = "세션 객체 삭제";
+            }else msg = "삭제할 객체 없음";
+        }else if(param.equals("add")){
+            session = req.getSession();
+            session.setAttribute("msg", "메시지 입니다.");
+            msg = "세션 객체에 데이터 등록 완료";
+        }else if(param.equals("get")){
+            session = req.getSession();
+            if(session != null){
+                String str = (String)session.getAttribute("msg");
+                msg = str;
+            }else msg = "데이터를 추출할 세션 객체 존재하지 않음";
+        }else if(param.equals("remove")){
+            session = req.getSession(false);
+            if(session != null){
+                session.removeAttribute("msg");
+                msg = "세션 삭제 완료";
+            }else msg ="데이터를 삭제할 세션 객체 없음";
+        }else if(param.equals("replace")){
+            session = req.getSession();
+            session.setAttribute("msg", "새로운 메시지 입니다.");
+            msg = "세션 객체에 데이터 등록 완료";
+        }
+
+        out.print("처리 결과 : " + msg);
+        out.close();
+    }
+}
